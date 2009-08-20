d28b1204ab921ed129fbdd29c35950e977cd81f0
Eelco Lempsink <eml@tupil.com>
2009-08-20 08:30:28 +0200

    First working version

--- provpro.rb
+++ provpro.rb
@@ -9,16 +9,25 @@ login_url = 'http://developer.apple.com/iphone/login.action'
 agent.get(login_url) do |login_page|
  
   # Submit the login form
-  login_page.form_with(:name => /appleConnectForm/) do |form|
+  login_page.form_with(:name => "appleConnectForm") do |form|
     form["theAccountName"] = "etupil"
     form["theAccountPW"]   = "e^i09uT"
   end.submit
-  
+ 
   # TODO Check for invalid credentials
 
-  # After a succesful login we go the login page again and get redirected.
-  account_page   = agent.get(login_url)
-  program_portal = agent.click(account_page.link_with(:name => /iPhone Developer Program Portal/))
+  # After a succesful login we need to touch the login page again to establish a session
+  agent.get(login_url)
+  
+  # program_portal = agent.click(account_page.link_with(:text => /iPhone Developer Program Portal/))
+  # p program_portal
 
-  p program_portal
+  add_device_page = agent.get("http://developer.apple.com/iphone/manage/devices/add.action")
+  page = add_device_page.form_with(:name => "save") do |form|
+    form["deviceNameList[0]"]   = "Test"
+    form["deviceNumberList[0]"] = "12345678901234567890123456678901234567890"
+  end.submit
+
+  p page
+
 end
