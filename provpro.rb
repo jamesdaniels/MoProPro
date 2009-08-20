036611289a2363da1328869e4ed70b22da34d9e6
Eelco Lempsink <eml@tupil.com>
2009-08-20 14:56:58 +0200

    Getting more mature

--- provpro.rb
+++ provpro.rb
@@ -1,33 +1,93 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
 require 'rubygems'
 require 'mechanize'
 
-agent = WWW::Mechanize.new
+class ProvPro
 
-# TODO Use https?
-login_url = 'http://developer.apple.com/iphone/login.action'
+  # Since the website only uses JavaScript for validation(!), we need do this
+  # ourselves
+  def validate(name, udid)
+    if not name =~ /[\w\d ]+/
+      puts "Error: name can only contain alphanumeric characters and spaces"
+      exit
+    end
+    if not udid =~ /[a-fA-F\d]{40}/
+      puts "Error: UDID must be a 40 character hexadecimal string"
+      exit
+    end
+  end
 
-agent.get(login_url) do |login_page|
- 
-  # Submit the login form
-  login_page.form_with(:name => "appleConnectForm") do |form|
-    form["theAccountName"] = "etupil"
-    form["theAccountPW"]   = "e^i09uT"
-  end.submit
- 
-  # TODO Check for invalid credentials
+  def login
+    login_url = 'https://developer.apple.com/iphone/login.action'
+    form_name = 'appleConnectForm'
+    
+    @agent.get(login_url) do |login_page|
+     
+      # Submit the login form
+      login_page.form_with(:name => form_name ) do |form|
+        form["theAccountName"] = @username
+        form["theAccountPW"]   = @password
+      end.submit
+     
+      # TODO Check for invalid credentials
+    
+      # After a succesful login we need to touch the login page again to establish a session
+      # If we find the login form on this page, the login was not succesful
+       if not @agent.get(login_url).forms.find { |f| f.name == form_name }.nil?
+         STDERR << "Error: invalid credentials\n"
+         exit
+       end
+    end
+  end
 
-  # After a succesful login we need to touch the login page again to establish a session
-  agent.get(login_url)
-  
-  # program_portal = agent.click(account_page.link_with(:text => /iPhone Developer Program Portal/))
-  # p program_portal
+  def add_device(name, udid)
+    add_device_page = @agent.get("https://developer.apple.com/iphone/manage/devices/add.action")
+    page = add_device_page.form_with(:name => "save") do |form|
+      form["deviceNameList[0]"]   = name
+      form["deviceNumberList[0]"] = udid
+    end.submit
+    
+    p page
+  end
 
-  add_device_page = agent.get("http://developer.apple.com/iphone/manage/devices/add.action")
-  page = add_device_page.form_with(:name => "save") do |form|
-    form["deviceNameList[0]"]   = "Test"
-    form["deviceNumberList[0]"] = "12345678901234567890123456678901234567890"
-  end.submit
+  def initialize(args)
+    optionParser = OptionParser.new do |opts|
+      opts.banner = "Usage: provpro.rb [options] name UDID"
+    
+      opts.on("-u", "--username USERNAME", "ADC Username") do |u|
+        @username = u
+      end
+    
+      opts.on("-p", "--password PASSWORD", "ADC Password") do |p|
+        @password = p
+      end
+    end
+    
+    optionParser.parse!(args)
+    
+    if @username.nil? || @password.nil?
+      STDERR << "Error: no username or password provided\n"
+      STDERR << optionParser.help
+    end
 
-  p page
-
+    if args.size != 2
+      STDERR << "Error: not enough arguments\n"
+      STDERR << optionParser.help
+      exit
+    end
+    # else
+    
+    validate(args[0], args[1])
+    
+    @agent = WWW::Mechanize.new
+    login
+    add_device(args[0], args[1])
+  end
 end
+
+ProvPro.new(ARGV)
+
+# "etupil"
+# "e^i09uT"
